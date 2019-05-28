fac3bc316b1dbc274b72be6f6ba83413054de66d
ich777 <ich777@noreply.example.org>
2019-05-28 16:02:15 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-echo "---sleep---"
-sleep infinity
+echo "---Checking for 'papatcher.go'---"
+if [ ! -f ${SERVER_DIR}/papatcher.go ]; then
+	echo "---Downloading 'papatcher.go'---"
+	cd ${DATA_DIR}
+	wget -qO ${DATA_DIR}/papatcher.go https://raw.githubusercontent.com/planetary-annihilation/papatcher/master/papatcher.go
+fi
 
-wget -qO papatcher.go https://raw.githubusercontent.com/planetary-annihilation/papatcher/master/papatcher.go
 
+if [ -z "${PA_ACC_NAME}"]; then
+	echo "---Please enter your account name and restart the Container---"
+    
 (echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
 
 
