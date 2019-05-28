65542dcc43d5f3c7c392a21b1027bf4b22aa72bf
ich777 <ich777@noreply.example.org>
2019-05-28 13:40:34 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,13 @@
 echo "---sleep---"
 sleep infinity
 
+wget -qO papatcher.go https://raw.githubusercontent.com/planetary-annihilation/papatcher/master/papatcher.go
+
+(echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
+
+
+./server --allow-lan --game-mode PAExpansion1:lobby --server-name "chipsServer" --mt-enabled --max-players 12 --headless
+
 echo "---Preparing Server---"
 
 chmod -R 770 ${DATA_DIR}
