923cb8435747cd0ee2806fad17b02cdaf65a0dc9
ich777 <ich777@noreply.example.org>
2019-05-29 09:38:23 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,25 +1,93 @@
 #!/bin/bash
-echo "---Checking for 'papatcher.go'---"
-if [ ! -f ${SERVER_DIR}/papatcher.go ]; then
-	echo "---Downloading 'papatcher.go'---"
-	cd ${DATA_DIR}
-	wget -qO ${DATA_DIR}/papatcher.go https://raw.githubusercontent.com/planetary-annihilation/papatcher/master/papatcher.go
+if [ "${UPDATE_ON_START}" == "true" ]; then
+	if [ -f ${SERVER_DIR}/papatcher.go ]; then
+    	rm ${SERVER_DIR}/papatcher.go
+    fi
+	if [ ! -f ${SERVER_DIR}/papatcher.go ]; then
+    	echo
+    	echo "-------------------------------------------------"
+    	echo "-----------'Update on Start' enabled-------------"
+    	echo "---Downloading new copy of file 'papatcher.go'---"
+        echo "-------------------------------------------------"
+		echo
+		cd ${SERVER_DIR}
+		wget -qO ${SERVER_DIR}/papatcher.go https://raw.githubusercontent.com/planetary-annihilation/papatcher/master/papatcher.go
+	fi
+	if [ -z "${PA_ACC_NAME}"]; then
+		echo "---Please enter your PA account name and restart the Container---"
+    	sleep infinity
+	fi
+	if [ -z "${PA_ACC_PWD}"]; then
+		echo "---Please enter your PA account password and restart the Container---"
+    	sleep infinity
+	fi
+else
+	echo "---Checking for 'papatcher.go'---"
+	if [ ! -f ${SERVER_DIR}/papatcher.go ]; then
+		echo "---Downloading 'papatcher.go'---"
+		cd ${SERVER_DIR}
+		wget -qO ${SERVER_DIR}/papatcher.go https://raw.githubusercontent.com/planetary-annihilation/papatcher/master/papatcher.go
+	fi
+	if [ -z "${PA_ACC_NAME}"]; then
+		echo "---Please enter your PA account name and restart the Container---"
+    	sleep infinity
+	fi
+	if [ -z "${PA_ACC_PWD}"]; then
+		echo "---Please enter your PA account password and restart the Container---"
+    	sleep infinity
+	fi
 fi
 
-
-if [ -z "${PA_ACC_NAME}"]; then
-	echo "---Please enter your account name and restart the Container---"
-    
-(echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
-
-
-./server --allow-lan --game-mode PAExpansion1:lobby --server-name "chipsServer" --mt-enabled --max-players 12 --headless
+if [ "${GAME_STREAM}" == "stable" ]; then
+	if [ ! -d ${SERVER_DIR}/stable ]; then
+		echo "---Installing PA '${GAME_STREAM}'---"
+    	(echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
+    fi
+    if [ "${UPDATE_ON_START}" == "true" ]; then
+    	echo
+    	echo "---------------------------------------------"
+    	echo "---------'Update on Start' enabled-----------"
+    	echo "---Checking for PA '${GAME_STREAM}' update---"
+        echo "---------------------------------------------"
+        echo
+    	(echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
+    fi
+elif [ "${GAME_STREAM}" == "PTE" ]; then
+	if [ ! -d ${SERVER_DIR}/PTE ]; then
+		echo "---Installing PA '${GAME_STREAM}'---"
+    	(echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
+    fi
+    if [ "${UPDATE_ON_START}" == "true" ]; then
+    	echo
+    	echo "---------------------------------------------"
+    	echo "---------'Update on Start' enabled-----------"
+    	echo "---Checking for PA '${GAME_STREAM}' update---"
+        echo "---------------------------------------------"
+        echo
+    	(echo "${PA_ACC_NAME}"; sleep 1s; echo ${PA_ACC_PWD}) | go run papatcher.go -stream=${GAME_STREAM} -dir=${SERVER_DIR}
+    fi
+else
+	echo
+    echo "--------------------------------------------"
+    echo "----Something went wrong! Please enter------"
+    echo "--------the prefered 'Game Stream'!---------"
+    echo "-----------Available options are:'----------"
+    echo "-----'stable' and 'PTE' (without quotes)----"
+    echo "----------and restart the Container---------"
+    echo "--------------------------------------------"
+    echo
+    sleep infinity
+fi
 
 echo "---Preparing Server---"
-
 chmod -R 770 ${DATA_DIR}
 
-
 echo "---Starting Server---"
-cd ${SERVER_DIR}
-${SERVER_DIR}/ ${GAME_PARAMS}
+if [ "${GAME_STREAM}" == "stable" ]; then
+	cd ${SERVER_DIR}/stable
+	${SERVER_DIR}/server --game-mode ${GAME_MODE} --server-name "${SERVER_NAME}" --mt-enabled --max-players ${MAX_PLAYERS} --headless ${GAME_PARAMS} --allow-lan --port ${GAME_PORT} --password ${SERVER_PASSWORD} ${GAME_PARAMS}
+fi
+if [ "${GAME_STREAM}" == "PTE" ]; then
+	cd ${SERVER_DIR}/stable
+	${SERVER_DIR}/server --game-mode ${GAME_MODE} --server-name "${SERVER_NAME}" --mt-enabled --max-players ${MAX_PLAYERS} --headless ${GAME_PARAMS} --allow-lan --port ${GAME_PORT} --password ${SERVER_PASSWORD} ${GAME_PARAMS}
+fi
